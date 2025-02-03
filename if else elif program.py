743276f56a9519ebb26c42d7b015081d58cce460
Sharan <32306006+Sai1106175@users.noreply.github.com>
2025-02-03 23:43:02 +0530

    Create if else elif program.py

---
+++ if else elif program.py
@@ -0,0 +1,10 @@
+userNumber = int(input("Please enter the number"))
+
+if(userNumber < 0):
+  print("The entered number is negative")
+elif(userNumber==0):
+  print("The entered number is Zero")
+else:
+  print("The entered number is Positive")
+
+print("Thankyou for your program.")
