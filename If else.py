0af25ab2fb98b3934be4e053f0ade64ec2b025a9
Sharan <32306006+Sai1106175@users.noreply.github.com>
2025-02-03 23:29:58 +0530

    Create If else.py

---
+++ If else.py
@@ -0,0 +1,7 @@
+a = int(input("Enter Your Age"))
+print("Your age is :", a)
+
+if(a < 18):
+  print("Your age is less then 18 you cant drive")
+else:
+  print("Your Age is greater then 18 You can drive")
