a27a20a48aecf1659003a79786e8e45cc7dea4b2
Sharan <32306006+Sai1106175@users.noreply.github.com>
2025-02-05 10:48:36 +0530

    Update GM Calculator using Def Function.py

--- GM Calculator using Def Function.py
+++ GM Calculator using Def Function.py
@@ -1,3 +1,17 @@
+'''
+Definition of a Function
+A function in Python is a reusable block of code that performs a specific task. It helps in code organization, reusability, and modular programming.
+
+Types of Functions in Python
+Built-in Functions – Predefined functions in Python, e.g., print(), len(), sum(), max(), min(), etc.
+User-defined Functions – Functions created by the user to perform specific tasks.
+Lambda Functions – Anonymous functions that can have only one expression.
+Recursive Functions – Functions that call themselves for repetitive tasks.
+
+'''
+
+
+
 def calGM (a,b):
   GrossMargin = (a-b)/a
   GrossMargin = GrossMargin*100
